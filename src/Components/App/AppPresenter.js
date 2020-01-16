06f839ba6dbb3aef1f88a244d980d8279fc1f74b
oks234 <bestspx@gmail.com>
2020-01-16 13:39:17 +0900

    #6 Rendering the Notifications

--- src/Components/App/AppPresenter.js
+++ src/Components/App/AppPresenter.js
@@ -1,13 +1,28 @@
-import React, { Fragment } from "react";
-import Header from "Components/Header";
-import Flex from "styled-flex-component";
-import Notification from "Components/Notification";
+import React, { Fragment } from 'react';
+import Header from 'Components/Header';
+import Flex from 'styled-flex-component';
+import Notification from 'Components/Notification';
+import Store from 'store';
 
 const AppPresenter = () => (
   <Fragment>
     <Header />
     <Flex alignCenter full column>
-      <Notification id={1} text={"Hello"} seen={false} />
+      <Store.Consumer>
+        {store => {
+          return Object.keys(store.notifications).map(key => {
+            const notification = store.notifications[key];
+            return (
+              <Notification
+                key={notification.id}
+                id={notification.id}
+                text={notification.text}
+                seen={notification.seen}
+              />
+            );
+          });
+        }}
+      </Store.Consumer>
     </Flex>
   </Fragment>
 );
